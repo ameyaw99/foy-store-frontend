9ee25678c85e492ca8c2163834111b210f6a93f2
bennydan7 <owusub243@gmail.com>
2024-03-06 08:31:48 +0000

    modified app page, added item dbs , added theme

---
+++ src/Item.js
@@ -0,0 +1,27 @@
+import beanie1 from "./assets/foyImages/01(beanie).png";
+
+const Items = [
+  {
+    id: "1",
+    name: "Product 1",
+    shortDescription: "Short description for Product 1",
+    longDescription: "Long description for Product 1",
+    price: 19.99,
+    image: beanie1,
+    // image2: "product1_alt.jpg",
+    category: "Category A",
+  },
+  {
+    id: "2",
+    name: "Product 2",
+    shortDescription: "Short description for Product 2",
+    longDescription: "Long description for Product 2",
+    price: 29.99,
+    image: beanie1,
+    // image2: "product2_alt.jpg",
+    category: "Category B",
+  },
+  // Add more items as needed
+];
+
+export default Items;
