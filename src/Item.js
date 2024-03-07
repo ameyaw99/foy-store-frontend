22f0f72dd625f496c47d095597be4b43b2a5e831
bennydan7 <owusub243@gmail.com>
2024-03-07 03:46:01 +0000

    added tailwind styles, added dependencies

--- src/Item.js
+++ src/Item.js
@@ -1,27 +1,75 @@
 import beanie1 from "./assets/foyImages/01(beanie).png";
+import beanie2 from "./assets/foyImages/02(beanie).png";
+import cap1 from "./assets/foyImages/03(cap).png";
+import cap2 from "./assets/foyImages/04(cap).png";
+import cap3 from "./assets/foyImages/05(cap).png";
+import tee1 from "./assets/foyImages/06(tee).png";
 
 const Items = [
   {
     id: "1",
-    name: "Product 1",
-    shortDescription: "Short description for Product 1",
-    longDescription: "Long description for Product 1",
-    price: 19.99,
-    image: beanie1,
+    name: "FOY GRAFFITI TEE",
+    shortDescription: "[BLACK]",
+    longDescription: " ",
+    price: 30,
+    image: tee1,
     // image2: "product1_alt.jpg",
-    category: "Category A",
+    category: "Tees",
   },
   {
     id: "2",
-    name: "Product 2",
-    shortDescription: "Short description for Product 2",
+    name: "FOY TRUCKER HAT",
+    shortDescription: "[BLACK]",
+    longDescription:
+      "Introducing our stylish collection of hats, available in classic black. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
+    price: 20,
+    image: cap2,
+    // image2: "product2_alt.jpg",
+    category: "Caps",
+  },
+  {
+    id: "3",
+    name: "FOY ALCATRAZ BEANIE",
+    shortDescription: "[BLACK & WHITE]",
     longDescription: "Long description for Product 2",
-    price: 29.99,
+    price: 15,
     image: beanie1,
     // image2: "product2_alt.jpg",
-    category: "Category B",
+    category: "Beanies",
   },
-  // Add more items as needed
+  {
+    id: "4",
+    name: "FOY TRUCKER HAT",
+    shortDescription: "[BABY BLUE]",
+    longDescription:
+      "LIntroducing our stylish collection of hats, available in classic black and white, as well as sophisticated black paired with a charming baby blue accent. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
+    price: 20,
+    image: cap1,
+    // image2: "product2_alt.jpg",
+    category: "Caps",
+  },
+  {
+    id: "5",
+    name: "FOY ALCATRAZ BEANIE",
+    shortDescription: "[BROWN & BEIGE]",
+    longDescription:
+      "High-quality image showcasing our alcatraz beanies in various elegant color combinations: brown and beige",
+    price: 15,
+    image: beanie2,
+    // image2: "product2_alt.jpg",
+    category: "Beanies",
+  },
+  {
+    id: "6",
+    name: "FOY TRUCKER HAT",
+    shortDescription: "[BLACK & WHITE]",
+    longDescription:
+      "Introducing our stylish collection of hats, available in classic black and white. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
+    price: 20,
+    image: cap3,
+    // image2: "product2_alt.jpg",
+    category: "Caps",
+  },
 ];
 
 export default Items;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/scenes/global/Footer.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
