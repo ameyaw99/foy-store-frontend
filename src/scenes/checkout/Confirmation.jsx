b549430534f81ecd4e25c07515ff3c860eae5b8d
bennydan7 <owusub243@gmail.com>
2024-03-19 16:07:50 +0000

    modified checkout page

--- src/scenes/checkout/Confirmation.jsx
+++ src/scenes/checkout/Confirmation.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Box, Container } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 const Confirmation = () => {
-  return <div>Confirmation</div>;
+  return (
+    <Container>
+      <Box my={10}>
+        {" "}
+        {/* Adjust the margin top and bottom */}
+        <Alert severity="success">
+          <AlertTitle>Success</AlertTitle>
+          You have successfully made an Order —{" "}
+          <strong>Congrats on Making your Purchase</strong>
+        </Alert>
+      </Box>
+    </Container>
+  );
 };
 
 export default Confirmation;
