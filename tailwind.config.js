c55fbeb1730716e6678828fb7b8222b9f9ea13e3
bennydan7 <owusub243@gmail.com>
2024-03-19 16:05:56 +0000

    created paths for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/scenes/global/Footer.jsx"],
+  content: ["./src/scenes/global/Footer.jsx", "./src/components/Search.jsx"],
   theme: {
     extend: {},
   },
