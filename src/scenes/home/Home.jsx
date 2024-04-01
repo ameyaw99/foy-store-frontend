d6f6c1840a856c05b236c9d92eea32914441fe9b
bennydan7 <owusub243@gmail.com>
2024-04-01 21:13:51 +0000

    mpdified changes

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div>
       <ShoppingList />
-      {/* <Subscribe /> */}
-      <Subscribe1 />
+      <Subscribe />
+      {/* <Subscribe1 /> */}
     </div>
   );
 };
