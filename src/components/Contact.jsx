d48adfd8ffc4679329ab8c9ec8c5de88159f57a1
bennydan7 <owusub243@gmail.com>
2024-04-09 18:49:44 +0000

    modified the pages to display a dark backfround

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,28 +3,26 @@ import { Container, Typography, Box, Link } from "@mui/material";
 
 const Contact = () => {
   return (
-    <Container maxWidth="md">
-      <Typography
-        variant="h3"
-        gutterBottom
-        sx={{ marginTop: 10, marginBottom: 2, textAlign: "center" }}
-      >
-        CONTACT
-      </Typography>
-      <Typography variant="h6" gutterBottom sx={{ textAlign: "center" }}>
-        INFORMATION
-      </Typography>
-      <Typography paragraph sx={{ textAlign: "center" }}>
-        Questions about the Terms of Service should be sent to us at{" "}
-        <Link
-          href="mailto:focusonyourself03@gmail.com"
-          className="text-blue-600"
+    <div className="bg-neutral-950 text-white -mt-5 -mb-7">
+      <Container maxWidth="md">
+        <Typography
+          variant="h3"
+          gutterBottom
+          sx={{ marginTop: 10, marginBottom: 2, textAlign: "center" }}
         >
-          focusonyourself03@gmail.com
-        </Link>
-        .
-      </Typography>
-    </Container>
+          CONTACT
+        </Typography>
+        <Typography variant="h6" gutterBottom sx={{ textAlign: "center" }}>
+          INFORMATION
+        </Typography>
+        <Typography paragraph sx={{ textAlign: "center" }}>
+          Questions about the Terms of Service should be sent to us at{" "}
+          <Link href="mailto:focusonyourself03@gmail.com" color="inherit">
+            focusonyourself03@gmail.com
+          </Link>
+        </Typography>
+      </Container>
+    </div>
   );
 };
 
