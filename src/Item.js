57fea63a8d296876f440b57ad408de4dbb139592
ameyaw99 <166561353+ameyaw99@users.noreply.github.com>
2024-05-09 21:36:52 +0000

    Update Item.js

--- src/Item.js
+++ src/Item.js
@@ -1,5 +1,5 @@
-import beanie1 from "./assets/foyImages/01(beanie).png";
-import beanie2 from "./assets/foyImages/02(beanie).png";
+// import beanie1 from "./assets/foyImages/01(beanie).png";
+// import beanie2 from "./assets/foyImages/02(beanie).png";
 import cap1 from "./assets/foyImages/03(cap).png";
 import cap2 from "./assets/foyImages/04(cap).png";
 import cap3 from "./assets/foyImages/05(cap).png";
@@ -31,16 +31,16 @@ const Items = [
     image2: cap2Front,
     category: "Caps",
   },
-  {
-    id: "3",
-    name: "FOY ALCATRAZ BEANIE",
-    shortDescription: "[BLACK & WHITE]",
-    longDescription: "Long description for Product 2",
-    price: 15,
-    image: beanie1,
-    image2: beanie1,
-    category: "Beanies",
-  },
+  // {
+  //   id: "3",
+  //   name: "FOY ALCATRAZ BEANIE",
+  //   shortDescription: "[BLACK & WHITE]",
+  //   longDescription: "Long description for Product 2",
+  //   price: 15,
+  //   image: beanie1,
+  //   image2: beanie1,
+  //   category: "Beanies",
+  // },
   {
     id: "4",
     name: "FOY TRUCKER HAT",
@@ -52,17 +52,17 @@ const Items = [
     image2: cap1Front,
     category: "Caps",
   },
-  {
-    id: "5",
-    name: "FOY ALCATRAZ BEANIE",
-    shortDescription: "[BROWN & BEIGE]",
-    longDescription:
-      "High-quality image showcasing our alcatraz beanies in various elegant color combinations: brown and beige",
-    price: 15,
-    image: beanie2,
-    image2: beanie2,
-    category: "Beanies",
-  },
+  // {
+  //   id: "5",
+  //   name: "FOY ALCATRAZ BEANIE",
+  //   shortDescription: "[BROWN & BEIGE]",
+  //   longDescription:
+  //     "High-quality image showcasing our alcatraz beanies in various elegant color combinations: brown and beige",
+  //   price: 15,
+  //   image: beanie2,
+  //   image2: beanie2,
+  //   category: "Beanies",
+  // },
   {
     id: "6",
     name: "FOY TRUCKER HAT",
