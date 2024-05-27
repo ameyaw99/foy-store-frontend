62f06ca7192420632287ce4a9329138ce696d02d
ameyaw99 <166561353+ameyaw99@users.noreply.github.com>
2024-05-27 11:40:35 +0000

    Update Item.js

--- src/Item.js
+++ src/Item.js
@@ -1,14 +1,3 @@
-import tee3 from "./assets/foyImages/01(tee).png";
-import tee4 from "./assets/foyImages/02(tee).png";
-import cap1 from "./assets/foyImages/03(cap).png";
-import cap2 from "./assets/foyImages/04(cap).png";
-import cap3 from "./assets/foyImages/05(cap).png";
-import tee1 from "./assets/foyImages/06(tee).png";
-import tee2 from "./assets/foyImages/07(tee).png";
-import cap1Front from "./assets/foyImages/10(cap).png";
-import cap2Front from "./assets/foyImages/09(cap).png";
-import cap3Front from "./assets/foyImages/08(cap).png";
-
 const Items = [
   {
     id: "1",
@@ -16,8 +5,6 @@ const Items = [
     shortDescription: "[BLACK]",
     longDescription: " ",
     price: 30,
-    image: tee1,
-    image2: tee2,
     category: "Tees",
   },
   {
@@ -27,8 +14,7 @@ const Items = [
     longDescription:
       "Introducing our stylish collection of hats, available in classic black. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
     price: 20,
-    image: cap2,
-    image2: cap2Front,
+
     category: "Caps",
   },
   {
@@ -37,9 +23,7 @@ const Items = [
     shortDescription: "[WHITE]",
     longDescription: "",
     price: 30,
-    image: tee3,
-    image2: tee4,
-    category: "Beanies",
+    category: "Tees",
   },
   {
     id: "4",
@@ -48,21 +32,19 @@ const Items = [
     longDescription:
       "Introducing our stylish collection of hats, available in classic black and white, as well as sophisticated black paired with a charming baby blue accent. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
     price: 20,
-    image: cap1,
-    image2: cap1Front,
+
     category: "Caps",
   },
   {
-    id: "6",
+    id: "5",
     name: "FOY TRUCKER HAT",
     shortDescription: "[BLACK & WHITE]",
     longDescription:
       "Introducing our stylish collection of hats, available in classic black and white. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
     price: 20,
-    image: cap3,
-    image2: cap3Front,
+
     category: "Caps",
   },
 ];
 
-export default Items;
+module.exports = Items;
